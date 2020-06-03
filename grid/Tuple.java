4b03f568e63b7e18f890e953c132b54272cfec0b
s3728807 <vdvillaflores@gmail.com>
2020-06-04 02:49:31 +1000

    completed tuple class

--- grid/Tuple.java
+++ grid/Tuple.java
@@ -4,12 +4,38 @@ public class Tuple {
     private int row;
     private int col;
     private int value;
+    private int boxes;
 
     public Tuple(int r, int c, int v)
     {
         row = r;
         col = c;
         value = v;
+        boxes = -1;
+    }
+
+    public Tuple(int r, int c, int v, int b)
+    {
+        row = r;
+        col = c;
+        value = v;
+        boxes = b;
+    }
+
+    public Tuple(int v, int b)
+    {
+        value = v;
+        boxes = b;
+        row = -1;
+        col = -1;
+    }
+    
+    public int getBoxes() {
+        return this.boxes;
+    }
+
+    public void setBoxes(int boxes) {
+        this.boxes = boxes;
     }
 
     public int getRow()
