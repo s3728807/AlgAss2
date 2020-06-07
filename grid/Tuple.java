dbaaebf11f7f2ffdd4961d462276a54ff1ec133b
s3728807 <vdvillaflores@gmail.com>
2020-06-07 22:53:04 +1000

    made constructor that includes cage num

--- grid/Tuple.java
+++ grid/Tuple.java
@@ -5,6 +5,7 @@ public class Tuple {
     private int col;
     private int value;
     private int boxes;
+    private int cage;
 
     public Tuple(int r, int c, int v)
     {
@@ -20,6 +21,15 @@ public class Tuple {
         col = c;
         value = v;
         boxes = b;
+    }
+
+    public Tuple(int r, int c, int v, int b, int ca)
+    {
+        row = r;
+        col = c;
+        value = v;
+        boxes = b;
+        cage = ca;
     }
 
     public Tuple(int v, int b)
@@ -62,5 +72,27 @@ public class Tuple {
     public void setVal(int v)
     {
         value = v;
+    }
+	public void setRow(int row) {
+        this.row = row;
+    }
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getCage() {
+        return this.cage;
+    }
+
+    public void setCage(int cage) {
+        this.cage = cage;
     }
 }
